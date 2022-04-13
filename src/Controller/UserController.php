daa8dc3ce4f96f4e25d5a5b6b66ec2fd5c96bf7a
Rabab Hassouna <rababahmed88@gmail.com>
2022-04-14 01:30:05 +0200

    refactor register API (#3)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,25 +4,32 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\View\View;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
-class UserController extends AbstractController
+class UserController extends AbstractFOSRestController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(Request $request,UserPasswordHasherInterface $passwordHasher,EntityManagerInterface $em,JWTTokenManagerInterface $JWTManager): Response
+    /**
+     * @Route("/user", methods={"POST"})
+     * @ParamConverter("user", converter="fos_rest.request_body")
+     */
+
+    public function registerUser(User $user,UserPasswordHasherInterface $passwordHasher,EntityManagerInterface $em,JWTTokenManagerInterface $JWTManager,ConstraintViolationListInterface $validationErrors): Response
     {
-        $email = $request->get('email');
-        $password = $request->get('password');
-        $user = new User();
-        $user->setEmail($email);
+        if(count($validationErrors) > 0){
+            return $this->handleView(View::create($validationErrors,Response::HTTP_BAD_REQUEST));
+        }
         $hashedPassword = $passwordHasher->hashPassword(
             $user,
-            $password
+            $user->getPassword()
         );
         $user->setPassword($hashedPassword);
         $em->persist($user);
